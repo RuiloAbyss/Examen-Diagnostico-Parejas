e7f4cbd2cb8247a0d7da4692b8b1d1896e5226d7
RuiloAbyss <ruilo.abisai@outlook.com>
2025-08-26 22:48:55 -0700

    Ejercicio 2

--- Ejercicio 2/Ejercicio2.js
+++ Ejercicio 2/Ejercicio2.js
@@ -1,6 +1,9 @@
-function ReversionCadena() {
+function ReversionCadena(){
     let cadRev = prompt("escribe algo...: ");
+    console.log("cadena original: " + cadRev); 
     cadRev = cadRev.split('').reverse().join('');
+    /*split convierte la cadena en un array, reverse invierte el array y 
+    join une el array en una cadena (usando '' como separador)*/
     console.log("cadena invertida: " + cadRev);
 }
 ReversionCadena();
